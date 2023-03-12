58cb16c47c516c96c7f5115cb0b9c714a3c1a34e
Igomigo <igomigofatai@gmail.com>
2023-03-12 14:17:08 -0700

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    list = my_list[:]
     if 0 <= idx < len(my_list):
+        list = my_list[:]
         my_list[idx] = element
         return(list)
     else:
