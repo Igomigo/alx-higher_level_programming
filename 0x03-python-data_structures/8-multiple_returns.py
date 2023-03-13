03b9d8dc3a1194aaf6e2469437967a93dd99c808
Igomigo <igomigofatai@gmail.com>
2023-03-13 06:38:41 -0700

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,9 @@
 
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
-    tuple = (length, first_char)
     if length == 0:
         result = (0, None)
         return result
-    return tuple
+    else:
+        tuple = (length, sentence[0])
+        return tuple
