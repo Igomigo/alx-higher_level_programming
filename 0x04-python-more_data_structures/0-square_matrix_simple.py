7ca34eb13a794ec936a3fd71effdcd4b17df3d0e
Igomigo <igomigofatai@gmail.com>
2023-03-16 09:31:37 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     new_mat = []
     for m in matrix:
         new_mat.append(list(map(lambda x: x ** 2, m)))
-    print("{}".format(new_mat))
+    return new_mat
