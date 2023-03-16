d5d51a93ecac9a16e0f609e11de30b6698a34504
Igomigo <igomigofatai@gmail.com>
2023-03-16 12:01:51 -0700

    Search and Replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    my_list[search] = replace
+    element = my_list.index(search)
+    my_list[element] = replace
     return my_list
