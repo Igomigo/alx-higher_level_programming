2a8822ac4d8d3502a2c03f157f598a03ba8d57d4
Igomigo <igomigofatai@gmail.com>
2023-03-17 12:56:51 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(my_dict):
-    max_v = None
-    max_k = None
-    for k, v in my_dict.items():
-        if max_v is None or v > max_v:
-            max_v = v
-            max_k = k
-        else:
-            return None
-    return max_k
+    if my_dict:
+        return None
+    else:
+        max_v = None
+        max_k = None
+        for k, v in my_dict.items():
+            if max_v is None or v > max_v:
+                max_v = v
+                max_k = k
+        return max_k
