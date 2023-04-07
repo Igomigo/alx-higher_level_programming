32f1c53ecd1ddc8e6ebd804ff8633033bb8ed2ed
Igomigo <igomigofatai@gmail.com>
2023-04-07 03:43:14 -0700

    0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+A module that contains a function that adds two integers and
+returns the result.
+
+function:
+    add_integer(a, b)
+"""
+
+
+def add_integer(a=0, b=0):
+    """A function that adds two integers"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
