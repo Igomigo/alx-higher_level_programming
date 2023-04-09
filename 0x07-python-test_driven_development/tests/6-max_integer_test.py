3059ab93a6060748ec6b0c40e3a5a08d2739254c
Igomigo <igomigofatai@gmail.com>
2023-04-09 10:17:16 -0700

    5. Max integer - Unittest

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""unittest for max_integer([..])
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """unittest class for the max_integer function"""
+    def test_module_docstring(self):
+        m = __import__('6-max_integer').__doc__
+        self.assertTrue(len(m) > 1)
+
+    def test_function_docstring(self):
+        func = __import__('6-max_integer').max_integer.__doc__
+        self.assertTrue(len(func) > 1)
+
+    def test_all_positive_integer_list(self):
+        result = max_integer([1, 3, 5, 7, 4])
+        self.assertEqual(result, 7)
+
+    def test_positive_negative_integer_list(self):
+        result = max_integer([1, -3, 4, -5,])
+        self.assertEqual(result, 4)
+
+    def test_all_negative_integer_list(self):
+        result = max_integer([-1, -2, -2, -3, -5])
+        self.assertEqual(result, -1)
+
+    def test_no_argument_passed(self):
+        result = max_integer()
+        self.assertEqual(result, None)
+
+    def test_single_integer_list(self):
+        result = max_integer([1])
+        self.assertEqual(result, 1)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_None_argument_passed(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_string_and_integers_args(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, "a", 3, 4, 5])
+
+if __name__ == '__main__':
+    unittest.main()
