964077d049b9b4aaa745fd6533b9f3f1379e701f
Igomigo <igomigofatai@gmail.com>
2023-04-11 16:04:43 -0700

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that contains the lookup() function
+"""
+
+
+def lookup(obj):
+    """
+    A function that returns the list of
+    available attributes and methods of an object
+    """
+    return dir(obj)
