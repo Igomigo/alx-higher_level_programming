e59abe8600cf18f456460740a0c2bd26503c37e9
Igomigo <igomigofatai@gmail.com>
2023-04-12 05:36:33 -0700

    4. Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A module that contains a function that checks
+if a class is a subclass of a parent class directly
+"""
+
+
+def inherits_from(obj, a_class):
+    """the function that checks"""
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
