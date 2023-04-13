4f18878b361156079877a3d8a5792019cb862f9f
Igomigo <igomigofatai@gmail.com>
2023-04-13 06:06:25 -0700

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A  module that contains a subclass of the Rectangle class
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ the square subclass that inherits from the
+    class Rectagle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] self.__size/self.__size
