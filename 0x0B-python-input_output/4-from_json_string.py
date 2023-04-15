c7ea5cb200a568b7f8e089c2b5a278c9ecf6b368
Igomigo <igomigofatai@gmail.com>
2023-04-15 15:02:49 -0700

    4. From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A module that contains a function that takes one argument
+def from_json_string(my_str).
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    A function that returns an object(python data structure)
+    represented by a JSON string.
+    """
+    return json.load(my_str)
