7606b02156b22f1fc422d5ca8864ebe03e5fec99
Igomigo <igomigofatai@gmail.com>
2023-05-18 07:47:17 -0700

    SQL code

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- A script to delete a database in MySQL server
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Script that creates a table in the MySQL server
+CREATE TABLE IF NOT EXISTS `first_table` (
+	id INT,
+	name VARCHAR(256)
+);
