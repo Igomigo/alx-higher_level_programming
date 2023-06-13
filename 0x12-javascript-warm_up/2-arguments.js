c4180e8974dd4aedbd616ca30b0091c67ecaadb5
Igomigo <igomigofatai@gmail.com>
2023-06-13 16:31:59 -0700

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let arg =  process.argv.length;
+if (arg < 3) {
+	console.log('No argument');
+} else if (arg === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
