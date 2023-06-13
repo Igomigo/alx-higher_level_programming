22257c9cd56acdf82b17fb5616bcff7892729cd8
Igomigo <igomigofatai@gmail.com>
2023-06-12 17:16:51 -0700

    More Queries, SQL

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,9 @@
 -- Creates database and table in MySQL server
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
---use the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use the database
 USE hbtn_0d_usa;
---to create the table
-CREATE TABLE IF NOT EXISTS `states` (
+-- to create the table
+CREATE TABLE IF NOT EXISTS states (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id)
