6c3b1d0a4d43f6fd7a4f9d1e116bd593077ebc00
igomigo <igomigofatai@gmail.com>
2023-06-15 09:38:53 -0700

    4. Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -22,14 +22,14 @@ class Rectangle {
   }
 
   rotate () {
-	  const temp = this.width;
-	  this.width = this.height;
-	  this.height = temp;
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
 
   double () {
-	  this.width = 2 * this.width;
-	  this.height = 2 * this.height;
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
   }
 }
 
