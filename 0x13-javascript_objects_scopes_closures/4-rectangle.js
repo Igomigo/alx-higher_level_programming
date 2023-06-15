165d99197d13017e4d85efe824c21470f2fc4298
igomigo <igomigofatai@gmail.com>
2023-06-15 09:32:59 -0700

    4. Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+/**
+ * class Rectangle
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+	  const temp = this.width;
+	  this.width = this.height;
+	  this.height = temp;
+  }
+
+  double () {
+	  this.width = 2 * this.width;
+	  this.height = 2 * this.height;
+  }
+}
+
+module.exports = Rectangle;
