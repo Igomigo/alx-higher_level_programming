30aa146e193f7de02cdf968ff353eb640c163db7
igomigo <igomigofatai@gmail.com>
2023-06-15 10:23:09 -0700

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,17 +5,17 @@ const PrevSquare = require('./5-square');
 class Square extends PrevSquare {
   charPrint (c) {
     if (c === undefined) {
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < this.width; i++) {
         let row = '';
-        for (let j = 0; j < this.size; j++) {
+        for (let j = 0; j < this.width; j++) {
           row += 'X';
         }
         console.log(row);
       }
     } else {
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < this.width; i++) {
         let row = '';
-        for (let j = 0; j < this.size; j++) {
+        for (let j = 0; j < this.width; j++) {
           row += c;
         }
         console.log(row);
