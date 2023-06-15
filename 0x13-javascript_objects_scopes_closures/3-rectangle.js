c1ceece7da4086c3849c13f583093dad24e137b3
Igomigo <igomigofatai@gmail.com>
2023-06-15 08:22:14 -0700

    3. Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-		if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-			return this;
-		}
-		this.width = w;
-		this.height = h;
-	}
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
 
-	print () {
-		for (let i = 0; i < this.height; i++) {
-			let row = "";
-			for (let j = 0; j < this.width; j++) {
-				row += 'X';
-			}
-			console.log(row);
-		}
-	}
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let row = "";
+      for (let j = 0; j < this.width; j++) {
+	row += 'X';
+      }
+      console.log(row);
+    }
+  }
 }
 
 module.exports = Rectangle;
