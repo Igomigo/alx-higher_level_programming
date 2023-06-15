4abf05325b5c27b614846dfc819c5c0fe6bfaca2
igomigo <igomigofatai@gmail.com>
2023-06-15 10:57:12 -0700

    7. Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * nbOccurences: returns the number of occurrences in a list
+ */
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      count++;
+    }
+  }
+  return count;
+};
