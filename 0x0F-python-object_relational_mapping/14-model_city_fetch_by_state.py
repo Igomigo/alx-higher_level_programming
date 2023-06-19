d23a340b360b2a34f5c4594acb2c0ef374444941
Igomigo <igomigofatai@gmail.com>
2023-06-19 15:06:46 -0700

    14. Cities in state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A script that prints all City objects from the database hbtn_0e_14_usa
+"""
+
+
+from model_city import Base, City
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Connecting to the database to perform some operations
+    """
+
+    db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(db_url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """Query the database"""
+    cities = session.query(State, City).filter(State.id == City.state_id)\
+            .order_by(City.id).all()
+    for state, city in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    """Commit the session"""
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This file contains the definition of the City class mapped
+to the database with the sqlalchemy ORM
+"""
+
+
+from sqlalchemy import Integer, Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    The City class.
+
+    Attributes:
+         __tablename__: Name of the table in the database.
+         id(Int): represents a column of an auto-generated,
+                unique integer, can’t be null and is a primary key.
+         name(String): represents a column of a string of 128
+                characters and can’t be null.
+         state_id(Int): represents a column of an integer, can’t be null
+                and is a foreign key to states.id.
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True, primary_key=True, unique=True)
+    name = Column(String(128))
+    state_id = Column(Integer, nullable=False, ForeignKey('State.id'))
