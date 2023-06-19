def45092e739f762ca0a31465026249ec28a8986
Igomigo <igomigofatai@gmail.com>
2023-06-19 06:13:54 -0700

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name 
+Script that lists all states with a name
 starting with `N` from the
 database `hbtn_0e_0_usa`
 """
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     db_cursor = db_conn.cursor()
 
     db_cursor.execute(
-    "SELECT * FROM states WHERE states.name LIKE BINARY 'N%' \
+       "SELECT * FROM states WHERE states.name LIKE BINARY 'N%' \
             ORDER BY states.id ASC")
 
     rows_selected = db_cursor.fetchall()
