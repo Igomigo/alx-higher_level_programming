4bc4acd2a1fb61d1dd0b94397b22f2fa10c5ed8e
Igomigo <igomigofatai@gmail.com>
2023-06-19 09:02:42 -0700

    6. First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
-A script that defines a Base class and a State class
-which inherits from the Base class using the SQLAlchemy module
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
-from Base_model import Base, State
-from sqlalchemy import create_engine
 import sys
+from model_state import Base, State
 
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                   (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
