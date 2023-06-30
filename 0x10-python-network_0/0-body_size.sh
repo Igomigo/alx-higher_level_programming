7133bef1f4ff9dc2d6413262814584c40b2c1bee
Igomigo <igomigofatai@gmail.com>
2023-06-30 14:44:51 -0700

    Curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes using the `curl` command.
-
-curl -s $1 | wc -c
+# Bash script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response in bytes using
+# the `curl` command.
+curl -s "$1" | wc -c
