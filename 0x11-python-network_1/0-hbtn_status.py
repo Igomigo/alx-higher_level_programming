2993f20eb23ff80e25c15ca36c377551e0c88602
Igomigo <igomigofatai@gmail.com>
2023-07-02 13:36:43 -0700

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen(req) as response:
         body = response.read()
     print("Body response:")
-    print(f"\t\t- type: {type(body)}")
-    print(f"\t\t- content: {body}")
-    print(f"\t\t- utf8 content: {body.decode('utf-8')}")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
