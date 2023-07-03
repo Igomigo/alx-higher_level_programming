b496f1a2476619e178e46a4b2325b20c8474ad1b
Igomigo <igomigofatai@gmail.com>
2023-07-02 17:23:28 -0700

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,10 +11,7 @@ if __name__ == '__main__':
 
     url = argv[1]
     r = requests.get(url)
-    try:
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
         print(r.text)
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code >= 400:
-            print(f"Error code: {e.response.status_code}")
-        else:
-            pass
