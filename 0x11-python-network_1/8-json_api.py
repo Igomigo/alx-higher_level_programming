129f95657c8853243ee530319b16b1469b7523c2
Igomigo <igomigofatai@gmail.com>
2023-07-02 17:54:37 -0700

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
 """
-Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
-"""
-
+import sys
 import requests
-from sys import argv
 
 
-if __name__ == '_main__':
-    letter = "" if len(argv) == 1 else argv[1]
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {"q": letter}
 
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
